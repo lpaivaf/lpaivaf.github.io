4ad022f491abe9be4c186ffb18d1dbc9cac75d8c
Leandro Paiva <paivafleandro@gmail.com>
2024-07-07 22:49:40 +0100

    Funcionalidade slides  adicionada

--- JS/index.js
+++ JS/index.js
@@ -8,11 +8,11 @@ setInterval(function () {
     document.getElementById("radio" + contador).checked = true;
     contador ++;
 
-    if ( contador > 2) {
+    if ( contador > 3) {
         contador = 1;
     }
 
-}, 4000)
+}, 3000)
 
 
 // Whatsapp
